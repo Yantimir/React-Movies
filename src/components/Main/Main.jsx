d69d78f4c286c8575ce920e77005912df51e7f03
Yantimir <mingazitdinov@mail.ru>
2022-06-08 18:54:16 +0400

    осуществляет поиск по фильмам

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,23 +6,44 @@ import { Preloader } from "../Preloader/Preloader";
 export const Main = () => {
 
     const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [searchQuery, setSearchQuery] = useState("");
 
     useEffect(() => {
+        setIsLoading(!isLoading);
         fetch("http://www.omdbapi.com/?apikey=28dc1872&s=matrix")
             .then(res => res.json())
             .then(dataMovies => setMovies(dataMovies.Search))
-
+            .catch((err) => console.error(err))
+            .finally(() => {
+                setIsLoading(isLoading);
+            });
+        // eslint-disable-next-line
     }, []);
+
+    const handleSearchQuery = (inputValue) => {
+        setSearchQuery(inputValue);
+    }
+
+    const searchMovies = () => {
+        const data = searchQuery;
+        fetch(`http://www.omdbapi.com/?apikey=28dc1872&s=${data}`)
+            .then(res => res.json())
+            .then(dataMovies => setMovies(dataMovies.Search))
+            .catch((err)=> console.error(err))
+    }
 
     return (
         <main className="main__container content">
-            <Search/>
+            <Search 
+                handleSearchQuery={handleSearchQuery} 
+                searchMovies={searchMovies}
+            />
             {
                 movies?.length
                     ? <Movies movies={movies} />
                     : <Preloader />
             }
-
         </main>
     )
 }
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,19 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
 
-export const Search = () => {
+export const Search = ({ handleSearchQuery, searchMovies }) => {
 
-    const [search, setSearch] = useState("");
+    // const [searchQuery, setSearchQuery] = useState("");
+
+    // const handleKey = (event) => {
+    //     if (event.key === "Enter") {
+    //         handleSearchQuery(inputValue);
+    //     }
+    // }
+    const handleKey = (event) => {
+        if (event.key === "Enter") {
+            searchMovies();
+        }
+    }
+    // const searchMovies = (str) => {
+    //     fetch(`http://www.omdbapi.com/?apikey=28dc1872&s=${str}`)
+    //         .then(res => res.json())
+    //         .then(dataMovies => setMovies(dataMovies.Search))
+    //         .catch((err)=> console.error(err))
+    // }
 
     return (
         <div className="row">
             <div className="input-field">
                 <input
                     className="validate"
-                    placeholder="search"
+                    placeholder="search..."
                     type="search"
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
+                    // value={searchQuery}
+                    onChange={(e) => handleSearchQuery(e.target.value.toLowerCase())}
+                    onKeyDown={handleKey}
                 />
+                <button className="btn search-btn" onClick={searchMovies}>Search</button>
             </div>
         </div>
     )
